d6650a7e3f46bb6cb317ceb63b6b08e9f0382d98
uatemycookie <pairofkings30@gmail.com>
2023-03-11 23:18:49 -0600

    create dom tests, automatic change detection, and components with dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {KissService} from "./kiss.service";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test1';
+  inputValue = '';
+
+  constructor(private kissService: KissService) {
+
+  }
+
+  onClick() {
+    this.kissService.kiss()
+  }
 }
